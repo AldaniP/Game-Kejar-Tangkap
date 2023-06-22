e88c79fbadcf999583e864c6b64d1559d23dcfda
AldaniP <82463547+AldaniP@users.noreply.github.com>
2023-06-22 11:51:28 +0700

    Added timer and score

--- script.js
+++ script.js
@@ -2,48 +2,76 @@ document.addEventListener('DOMContentLoaded', () => {
     const player = document.getElementById('player');
     const target = document.getElementById('target');
     const gameBoard = document.getElementById('game-board');
-
+    const scoreDisplay = document.getElementById('score');
+    const timerDisplay = document.getElementById('timer');
+  
+    let score = 0;
+    let time = 30; // Waktu bermain dalam detik
+  
     document.addEventListener('keydown', movePlayer);
-
+  
     function movePlayer(event) {
-        const key = event.key;
-        const playerRect = player.getBoundingClientRect();
-        const targetRect = target.getBoundingClientRect();
-        const gameBoardRect = gameBoard.getBoundingClientRect();
-
-        if (key === 'ArrowUp' && playerRect.top > gameBoardRect.top) {
-            player.style.top = (playerRect.top - gameBoardRect.top - 10) + 'px';
-        } else if (key === 'ArrowDown' && playerRect.bottom < gameBoardRect.bottom) {
-            player.style.top = (playerRect.top - gameBoardRect.top + 10) + 'px';
-        } else if (key === 'ArrowLeft' && playerRect.left > gameBoardRect.left) {
-            player.style.left = (playerRect.left - gameBoardRect.left - 10) + 'px';
-        } else if (key === 'ArrowRight' && playerRect.right < gameBoardRect.right) {
-            player.style.left = (playerRect.left - gameBoardRect.left + 10) + 'px';
-        }
-
-        if (isColliding(playerRect, targetRect)) {
-            alert('Selamat! Anda berhasil mencapai target!');
-            resetTarget();
-        }
+      const key = event.key;
+      const playerRect = player.getBoundingClientRect();
+      const targetRect = target.getBoundingClientRect();
+      const gameBoardRect = gameBoard.getBoundingClientRect();
+  
+      const step = 10; // Jarak pergerakan pemain
+  
+      if (key === 'ArrowUp' && playerRect.top - step > gameBoardRect.top) {
+        player.style.top = (player.offsetTop - step) + 'px';
+      } else if (key === 'ArrowDown' && playerRect.bottom + step < gameBoardRect.bottom) {
+        player.style.top = (player.offsetTop + step) + 'px';
+      } else if (key === 'ArrowLeft' && playerRect.left - step > gameBoardRect.left) {
+        player.style.left = (player.offsetLeft - step) + 'px';
+      } else if (key === 'ArrowRight' && playerRect.right + step < gameBoardRect.right) {
+        player.style.left = (player.offsetLeft + step) + 'px';
+      }
+  
+      if (isColliding(playerRect, targetRect)) {
+        score++;
+        scoreDisplay.textContent = score;
+        alert('Selamat! Anda berhasil mencapai target!');
+        resetTarget();
+      }
     }
-
+  
     function isColliding(rect1, rect2) {
-        return rect1.left < rect2.right &&
-            rect1.right > rect2.left &&
-            rect1.top < rect2.bottom &&
-            rect1.bottom > rect2.top;
+      return rect1.left < rect2.right &&
+        rect1.right > rect2.left &&
+        rect1.top < rect2.bottom &&
+        rect1.bottom > rect2.top;
     }
-
+  
     function resetTarget() {
-        const maxX = gameBoard.clientWidth - target.offsetWidth;
-        const maxY = gameBoard.clientHeight - target.offsetHeight;
-
-        const randomX = Math.floor(Math.random() * maxX);
-        const randomY = Math.floor(Math.random() * maxY);
-
-        target.style.left = randomX + 'px';
-        target.style.top = randomY + 'px';
+      const maxX = gameBoard.clientWidth - target.offsetWidth;
+      const maxY = gameBoard.clientHeight - target.offsetHeight;
+  
+      const randomX = Math.floor(Math.random() * maxX);
+      const randomY = Math.floor(Math.random() * maxY);
+  
+      target.style.top = randomY + 'px';
+      target.style.left = randomX + 'px';
     }
-
-    setInterval(resetTarget, 3000); // Reset target setiap 3 detik
-});
+  
+    function updateTimer() {
+      timerDisplay.textContent = time;
+      time--;
+  
+      if (time < 0) {
+        endGame();
+      }
+    }
+  
+    function endGame() {
+      clearInterval(timerInterval);
+      document.removeEventListener('keydown', movePlayer);
+      alert(`Waktu bermain habis. Skor Anda: ${score}`);
+    }
+  
+    resetTarget();
+    scoreDisplay.textContent = score;
+  
+    const timerInterval = setInterval(updateTimer, 1000); // Perbarui timer setiap 1 detik
+  });
+  
